2724158a36ee5942bafb2e1a1b09218f87d0da80
hp <hp@DESKTOP-KD460RU>
2022-11-27 21:07:36 +0530

    new comit

--- src/main/java/handler/Test.java
+++ src/main/java/handler/Test.java
@@ -2,7 +2,7 @@ package handler;
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println("Hello World");
+	System.out.println("Hello World java");
 	System.out.println("github tutorial video");
 }
 }
