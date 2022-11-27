0481d11857b4657e26eadab42304e62ff23d13c4
hp <hp@DESKTOP-KD460RU>
2022-11-27 21:06:01 +0530

    new comit

--- src/main/java/handler/Test.java
+++ src/main/java/handler/Test.java
@@ -3,6 +3,6 @@ package handler;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Hello World");
-	System.out.println("github tutorial");
+	System.out.println("github tutorial video");
 }
 }
